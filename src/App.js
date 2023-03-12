1b1006577ae30372472a871e8b912d9c3d2d1c55
Mari Azh <Marzi.aghchay@gmail.com>
2023-03-12 11:16:51 +0000

    first step

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[toDoList ,setToDoList] = useState([]);
+  const[newTask,setNewTask] =useState("");
+
+ const taskHandler =(event)=>{
+  setNewTask(event.target.value)
+ };
+ const addTask=()=>{
+  const newArray =[...toDoList,newTask]
+  setToDoList(newArray);
+ };
+
   return (
-   <></>
+   <div>
+    <div>
+      <input onChange={taskHandler}/>
+      <button onClick={addTask}>Add Task</button>
+     
+    </div>
+    <div>
+      {toDoList.map((task)=>{
+        return <h1>{task}</h1>
+      })}
+    </div>
+   </div>
 )
 }
 
